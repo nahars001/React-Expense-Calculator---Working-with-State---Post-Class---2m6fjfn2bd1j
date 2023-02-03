0228d1887e466fa6c37a9ef8a3370135d2546e66
Nahar Singh <naharsingh151299@gmail.com>
2023-02-03 13:58:31 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
-
+  const expenseInput =  useRef("");
+  const[expenseList , setExpenseList] = useState([]);
+  const[totalExpense , setTotalexpense] = useState(0);
+  const addExpenseHandler = () => {
+    setExpenseList(prevList => {
+      let newList = [...prevList , expenseInput.current.value];
+      console.log(newList);
+      setTotalexpense(prevExp => prevExp + parseInt(expenseInput.current.value))  
+      return newList;
+    })    
+  }
   return (
     <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <input id="expense-input" ref={expenseInput} type='number'/>
+      <button id="expense-button" onClick={addExpenseHandler}>Add Expense</button>
       <div id="expense-list">
+        {expenseList.map(expense => (<li> {expense}</li>))}
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: {totalExpense}
       </div>
     </div>
   )
